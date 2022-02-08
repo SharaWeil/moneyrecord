43bc9d74802af03914b9ebe6aa62e6c540bdefaf
韩志华 <hanzhihua@mininglamp.com>
2022-02-08 17:25:01 +0800

    Initial commit

---
+++ src/test/java/com/hzh/moneyrecord/MoneyrecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzh.moneyrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyrecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
