e9443cbcd954dcf10403e1854c6120ea50cf9ec1
Administrator <2857656664@qq.com>
2022-02-08 21:17:10 +0800

    测试windows环境

--- src/main/java/com/hzh/moneyrecord/dao/Test.java
+++ src/main/java/com/hzh/moneyrecord/dao/Test.java
@@ -6,4 +6,7 @@ package com.hzh.moneyrecord.dao;
  * @createTime 2022年02月08日 18:21:00
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
 }
